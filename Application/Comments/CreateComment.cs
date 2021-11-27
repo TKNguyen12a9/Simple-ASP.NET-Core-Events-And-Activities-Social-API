d5d91bb04e081c74cfdf9626528634b99158b3e3
Kien Nguyen The <cudayanh@gmail.com>
2021-11-27 20:23:36 +0900

    removed not useful comment

--- Application/Comments/CreateComment.cs
+++ Application/Comments/CreateComment.cs
@@ -14,65 +14,62 @@ using Persistence;
 
 namespace Application.Comments
 {
-    public class CreateComment
-    {
-        public class Command : IRequest<Result<CommentDTO>>
-        {
-            public string Body { get; set; }
-            public Guid ActivityId { get; set; }
-        }
+	public class CreateComment
+	{
+		public class Command : IRequest<Result<CommentDTO>>
+		{
+			public string Body { get; set; }
+			public Guid ActivityId { get; set; }
+		}
 
-        public class CommandValidator : AbstractValidator<Command>
-        {
-            public CommandValidator()
-            {
-                RuleFor(x => x.Body).NotEmpty();
-            }
-        }
+		public class CommandValidator : AbstractValidator<Command>
+		{
+			public CommandValidator()
+			{
+				RuleFor(x => x.Body).NotEmpty();
+			}
+		}
 
-        public class Handler : IRequestHandler<Command, Result<CommentDTO>>
-        {
-            private readonly AppDbContext _dbContext;
-            private readonly IMapper _mapper;
-            private readonly IUserAccessor _userAccessor;
+		public class Handler : IRequestHandler<Command, Result<CommentDTO>>
+		{
+			private readonly AppDbContext _dbContext;
+			private readonly IMapper _mapper;
+			private readonly IUserAccessor _userAccessor;
 
-            public Handler(AppDbContext dbContext, IMapper mapper, IUserAccessor userAccessor)
-            {
-                _dbContext = dbContext;
-                _mapper = mapper;
-                _userAccessor = userAccessor;
-            }
+			public Handler(AppDbContext dbContext, IMapper mapper, IUserAccessor userAccessor)
+			{
+				_dbContext = dbContext;
+				_mapper = mapper;
+				_userAccessor = userAccessor;
+			}
 
-            public async Task<Result<CommentDTO>> Handle(Command request, CancellationToken cancellationToken)
-            {
-                var activity = await _dbContext.Activities.FindAsync(request.ActivityId);
+			public async Task<Result<CommentDTO>> Handle(Command request, CancellationToken cancellationToken)
+			{
+				var activity = await _dbContext.Activities.FindAsync(request.ActivityId);
 
-                if (activity == null) return null;
+				if (activity == null) return null;
 
-                var appUser = await _dbContext.Users
-                .Include(x => x.Photos)
-                .FirstOrDefaultAsync(x => x.UserName == _userAccessor.GetUserName());
+				var appUser = await _dbContext.Users
+				.Include(x => x.Photos)
+				.FirstOrDefaultAsync(x => x.UserName == _userAccessor.GetUserName());
 
-                if (appUser == null) return null;
+				if (appUser == null) return null;
 
-                var comment = new Comment
-                {
-                    AppUser = appUser,
-                    Activity = activity,
-                    Body = request.Body,
-                };
+				var comment = new Comment
+				{
+					AppUser = appUser,
+					Activity = activity,
+					Body = request.Body,
+				};
 
-                _dbContext.Comments.Add(comment);
+				_dbContext.Comments.Add(comment);
 
-                var result = await _dbContext.SaveChangesAsync() > 0;
+				var result = await _dbContext.SaveChangesAsync() > 0;
 
-                if (result) return Result<CommentDTO>.Success(_mapper.Map<CommentDTO>(comment));
+				if (result) return Result<CommentDTO>.Success(_mapper.Map<CommentDTO>(comment));
 
-                return Result<CommentDTO>.Failure("Failed when add comment.");
-            }
-        }
-    }
+				return Result<CommentDTO>.Failure("Adding comment failed.");
+			}
+		}
+	}
 }
-
-
-// {"type":3,"invocationId":"1","error":"Failed to invoke 'SendComment' due to an error on the server. InvalidDataException: Error binding arguments. Make sure that the types of the provided values match the types of the hub method being invoked."}	
